cfb1dcb61dbd4e21749110dfe04777c74231f806
aeon <jerry73204@gmail.com>
2025-06-15 13:30:40 +0800

    Disable pcl-rust examples whose FFI interface are not ready.

--- pcl-rust/examples/filters_demo.rs
+++ pcl-rust/examples/filters_demo.rs
@@ -3,7 +3,7 @@
 //! This example shows how to use the PassThrough filter to crop point clouds
 //! along specified dimensions.
 
-use pcl::{FilterXYZ, PassThroughXYZ, PclResult, PointCloudXYZ, filters::FilterXYZ};
+use pcl::{PclResult, PointCloudXYZ, filters::FilterXYZ, filters::PassThroughXYZ};
 
 fn main() -> PclResult<()> {
     println!("PCL Filters Demo");
